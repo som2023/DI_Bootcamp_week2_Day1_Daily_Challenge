8c3493823dc86e199ee2cb752cba9af3df6d565c
som2023 <josephsom44@gmail.com>
2023-01-24 22:39:43 +0000

    first commmit

---
+++ exercise1/fruit.js
@@ -0,0 +1,11 @@
+const fruits = ["Banana", "Apples", "Oranges", "Blueberries"];
+fruits.shift();
+console.log(fruits);
+fruits.sort();
+ console.log(fruits);
+ fruits.push("kiwi");
+ console.log(fruits);
+fruits.splice(0,1);
+console.log(fruits);
+fruits.reverse();
+console.log(fruits);
---
+++ exercise2/main.js
@@ -0,0 +1,2 @@
+const table = ["Banana", ["Apples", ["Oranges"], "Blueberries"]];
+console.log(table[1][1][0]);
